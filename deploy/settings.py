6fad645889ff258de7aa6115825629a582ec0bbd
Premsaiboga <bogapremsai26@gmail.com>
2025-11-11 13:29:24 +0530

    databasename solved

--- deploy/settings.py
+++ deploy/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_Name'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST':os.getenv('DB_HOST'),
